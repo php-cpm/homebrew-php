00be68a995caf8b29e5e4723f50bcc86517a5cd2
zouyi <zouyi@leying365.com>
2018-05-16 09:22:38 +0800

    combine to win

--- Abstract/abstract-php-extension.rb
+++ Abstract/abstract-php-extension.rb
@@ -16,100 +16,44 @@ class InvalidPhpizeError < RuntimeError
 end
 
 class PhpExtensionFormula < Formula
+  NAME_PATTERN = /^Php(?:([57])(\d+))?(.+)/
   def initialize(*)
     super
+
+    if build.without? "homebrew-php"
+      installed_php_version = nil
+      i = IO.popen("#{phpize} -v")
+      out = i.readlines.join("")
+      i.close
+      { 53 => 20090626, 54 => 20100412, 55 => 20121113, 56 => 20131106, 70 => 20151012, 71 => 20160303, 72 => 20170718 }.each do |v, api|
+        installed_php_version = v.to_s if out.match(/#{api}/)
+      end
+
+      raise UnsupportedPhpApiError if installed_php_version.nil?
+
+      required_php_version = php_branch.sub(".", "").to_s
+      unless installed_php_version == required_php_version
+        raise InvalidPhpizeError.new(installed_php_version, required_php_version)
+      end
+    end
   end
 
   def self.init
-    extension_dsl
-  end
+    depends_on "autoconf" => :build
 
-  def install
-    cd "ext/#{extension}"
-    system php_parent.bin/"phpize"
-    system "./configure", *configure_args
-    system "make"
-    (lib/module_path).install "modules/#{extension}.so"
-  end
-
-  def post_install
-    ext_config_path = etc/"php"/php_parent.php_version/"conf.d"/"ext-#{extension}.ini"
-    if ext_config_path.exist?
-      inreplace ext_config_path,
-        /#{extension_type}=.*$/, "#{extension_type}=#{opt_lib/module_path}/#{extension}.so"
+    option "without-homebrew-php", "Ignore homebrew PHP and use default instead"
+    option "without-config-file", "Do not install extension config file"
+    class_name = name.split("::").last
+    m = NAME_PATTERN.match(class_name)
+    if m.nil?
+      raise "Bad PHP Extension name for #{class_name}"
+    elsif m[1].nil?
+      parent_name = "php"
     else
-      ext_config_path.write <<~EOS
-        [#{extension}]
-        #{extension_type}=#{opt_lib/module_path}/#{extension}.so
-      EOS
-    end
-  end
-
-  test do
-    assert_match extension.downcase, shell_output("#{php_parent.opt_bin}/php -m").downcase,
-      "failed to find extension in php -m output"
-  end
-
-  private
-
-  def php_parent
-    self.class.php_parent
-  end
-
-  def extension
-    self.class.extension
-  end
-
-  def extension_type
-    # extension or zend_extension
-    "extension"
-  end
-
-  def module_path
-    extension_dir = Utils.popen_read("#{php_parent.opt_bin/"php-config"} --extension-dir").chomp
-    php_basename = File.basename(extension_dir)
-    "php/#{php_basename}"
-  end
-
-  def configure_args
-    self.class.configure_args
-  end
-
-  class << self
-    NAME_PATTERN = /^Php(?:([57])(\d+))?(.+)/
-    attr_reader :configure_args, :php_parent, :extension
-
-    def configure_arg(args)
-      @configure_args ||= []
-      @configure_args.concat(Array(args))
+      parent_name = "php@" + m.captures[0..1].join(".")
     end
 
-    def extension_dsl
-      class_name = name.split("::").last
-      m = NAME_PATTERN.match(class_name)
-      if m.nil?
-        raise "Bad PHP Extension name for #{class_name}"
-      elsif m[1].nil?
-        parent_name = "php"
-      else
-        parent_name = "php@" + m.captures[0..1].join(".")
-      end
-
-      @php_parent = Formula[parent_name]
-      @extension = m[3].gsub(/([a-z])([A-Z])/) do
-        Regexp.last_match(1) + "_" + Regexp.last_match(2)
-      end.downcase
-      @configure_args = %W[
-        --with-php-config=#{php_parent.opt_bin/"php-config"}
-      ]
-
-      homepage php_parent.homepage + extension
-      url php_parent.stable.url
-      send php_parent.stable.checksum.hash_type, php_parent.stable.checksum.hexdigest
-
-      depends_on "autoconf" => :build
-      depends_on parent_name
-    end
+    depends_on parent_name
   end
 
   def php_branch
